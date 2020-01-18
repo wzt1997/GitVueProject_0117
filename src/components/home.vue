d4e369a5399a75fb636134dea4f7215314c10546
wzt1997 <1005923098@qq.com>
2020-01-18 11:01:20 +0800

    登录功能完成

--- src/components/home.vue
+++ src/components/home.vue
@@ -2,13 +2,24 @@
 
 <template>
     <div>
+        <el-button type='info' @click='logout'>
+            退出
+        </el-button>
         home 组件
+
     </div>
 </template>
 
 <script>
 //新页面->新建组件、设置路由规则
 export default {
+    methods:{
+        logout(){
+            //退出的原理是清空token然后回到登录页
+            window.sessionStorage.clear();
+            this.$router.push("/login");
+        }
+    }
     
 }
 </script>
